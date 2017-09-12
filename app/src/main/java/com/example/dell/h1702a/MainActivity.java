83828035d6c048281e4ce8978290da80c3a2c801
zahngaodong <2528123133@qq.com>
2017-09-12 10:33:30 +0800

    diyici

---
+++ app/src/main/java/com/example/dell/h1702a/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.dell.h1702a;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initData();
+    }
+
+    private void initData() {
+
+    }
+}
